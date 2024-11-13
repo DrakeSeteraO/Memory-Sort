e63818b76c588f0043a48bf913eaa5898fc972e6
Drake Setera <47800977+DrakeSeteraO@users.noreply.github.com>
2024-11-13 11:20:10 -0500

    Add files via upload

--- MemorySort.py
+++ MemorySort.py
@@ -1,4 +1,4 @@
-def memorySort(lst: list) -> list:
+def memorySort2(lst: list) -> list:
     high, low = lst[0], lst[0]
     for l in lst:
         if l > high:
@@ -7,7 +7,18 @@ def memorySort(lst: list) -> list:
             low = l
     ran = high - low + 1
     output = [None] * ran
+    amount = dict()
     
     for l in lst:
         output[l - low] = l
-    return output
+        if l in amount.keys():
+            amount[l] += 1
+        else:
+            amount[l] = 1
+    
+    output2 = list()
+    for o in output:
+        if o is not None:
+              for _ in range(amount[o]):
+                  output2.append(o)     
+    return output2
